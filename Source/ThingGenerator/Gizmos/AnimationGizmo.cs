36b487414ef1b5de857b27e9def3b7a48b4acdf8
James <james@epicguru.co.uk>
2022-02-17 16:28:36 +0000

    More compatibility.

---
+++ Source/ThingGenerator/Gizmos/AnimationGizmo.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace AAM.Gizmos
+{
+    public class AnimationGizmo : Gizmo
+    {
+        public override GizmoResult GizmoOnGUI(Vector2 topLeft, float maxWidth, GizmoRenderParms parms)
+        {
+            var butRect = new Rect(topLeft.x, topLeft.y, this.GetWidth(maxWidth), 75f);
+
+            // Mouseover and highlight.
+            MouseoverSounds.DoRegion(butRect, SoundDefOf.Mouseover_Command);
+            if (parms.highLight)
+                QuickSearchWidget.DrawStrongHighlight(butRect.ExpandedBy(12f));
+
+            bool clicked = false;
+            bool mouseOver = Mouse.IsOver(butRect);
+
+            if (Widgets.ButtonInvisible(butRect, true))
+            {
+                clicked = true;
+            }
+
+            Widgets.DrawBox(butRect);
+
+            return new GizmoResult(clicked ? GizmoState.Interacted : mouseOver ? GizmoState.Mouseover : GizmoState.Clear);
+        }
+
+        public override float GetWidth(float maxWidth)
+        {
+            return maxWidth;
+        }
+    }
+}
---
+++ Source/ThingGenerator/Patches/Patch_Pawn_GetGizmos.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using AAM.Gizmos;
+using Verse;
+
+namespace AAM.Patches
+{
+    [HarmonyPatch(typeof(Pawn), nameof(Pawn.GetGizmos))]
+    public class Patch_Pawn_GetGizmos
+    {
+        static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> values)
+        {
+            foreach (var gizmo in values)
+                yield return gizmo;
+
+            yield return new AnimationGizmo();
+        }
+    }
+}
