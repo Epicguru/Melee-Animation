f50f490852a26fe768635bbb1fd82320ec4f1b07
James <james@epicguru.co.uk>
2022-03-12 15:25:11 +0000

    Added lightsaber patch project and assemblies.

---
+++ Source/LightsaberPatch/PatchCore.cs
@@ -0,0 +1,18 @@
+﻿
+using Verse;
+
+namespace AAM.LightsaberPatch
+{
+    public class PatchCore : Mod
+    {
+        public static void Log(string msg)
+        {
+            Core.Log($"[Lightsaber Patch] {msg}");
+        }
+
+        public PatchCore(ModContentPack content) : base(content)
+        {
+            Log("Loaded lightsaber patch!");
+        }
+    }
+}
